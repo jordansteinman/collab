8fbca874ff82066f95b7e5fc89405f912964b569
lacobham <146376068+lacobham@users.noreply.github.com>
2024-05-19 17:29:02 -0400

    Update warbler.js

--- warbler.js
+++ warbler.js
@@ -1,5 +1,4 @@
-/*JavaScript Codes */
-
+/*Expanding Buttons*/
 var acc = document.getElementsByClassName("ExpandingButton");
 var i;
 
@@ -14,8 +13,8 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
-/* java done by Rylee*/
-/*java needed on the responsiv navbar*/
+
+/*Java needed on the responsive navbar*/
 /*Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon*/
 function myFunction() {
   var x = document.getElementById("myTopnav");
@@ -25,24 +24,3 @@ function myFunction() {
     x.className = "topnav";
   }
 }
-/* Rylee ends*/
-
-/* Javascript to toggle between two images inspired by w3 schools tutorial https://www.w3schools.com/html/tryit.asp?filename=tryhtml_script_attribute Maya Ivimey*/
-function pickles(change) {
- 	 var pic;
-  	if (change == 0) {
-   	 pic = "Images/7pickles.jpg"
-  	} else {
-    	pic = "Images/9peicesofgum.jpg"
- 	 }
-  	document.getElementById('myImage').src = pic;
-	}
-function grapes(change) {
- 	 var pic;
-  	if (change == 0) {
-   	 pic = "Images/grapes.png"
-  	} else {
-    	pic = "Images/apples.jpg"
- 	 }
-  	document.getElementById('myImage2').src = pic;
-	}
