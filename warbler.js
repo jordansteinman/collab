0c488d0be14ad73c74147518ae5e77506c64d458
jordansteinman <161998955+jordansteinman@users.noreply.github.com>
2024-05-28 20:08:38 -0400

    Update warbler.js

--- warbler.js
+++ warbler.js
@@ -24,3 +24,19 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+//back-to-top button to bring back to the top once clicked https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+function topFunction() {
+    document.body.scrollTop = 0; // Safari
+    document.documentElement.scrollTop = 0; // Chrome, Firefox, Opera
+  }
+
+//back-to-top button appear once scrolled (https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_onscroll2)
+window.onscroll = function() {scrollTopButton()};
+function scrollTopButton() {
+  if (document.documentElement.scrollTop > 10) {
+    document.getElementById("topbutton").className = "top-show"; //changes class to top-show where display:block
+  } else {
+    document.getElementById("topbutton").className = "hide"; //keeps class to top-hide where display:none
+  }
+}
